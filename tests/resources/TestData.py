ee56c4bd93ecf5c5b6c964d6d0a1406e98bdf670
Danil <qoop.00@mail.ru>
2022-06-26 22:41:55 +0500

    add requirements

--- tests/resources/TestData.py
+++ tests/resources/TestData.py
@@ -15,64 +15,54 @@ def get_future_ticker(secid):
     data = response.json()['securities']['data']
     return data[0][14], data[0][17], data[0][6]
 
+def return_future_info(secid, _deposit, _count_contracts, _price_open, _price_stop):
+    initialmargin, stepprice, minstep = get_future_ticker(secid)
+    sizeposition = round((initialmargin * _count_contracts), 2)
+    if _price_open > _price_stop:
+        stoplosspoint = round((_price_open - _price_stop), 2)
+    else:
+        stoplosspoint = round((_price_stop - _price_open), 2)
+    potentialloss = round((stoplosspoint / minstep * stepprice * _count_contracts), 2)
+    potentiallosspercent = round((potentialloss / _deposit * 100), 2)
+    return {
+        'go': initialmargin,
+        'count_positions': sizeposition,
+        'stop_loss_p': stoplosspoint,
+        'potential_loss': potentialloss,
+        'percent_deposit': potentiallosspercent
+    }
+
 def get_stock_ticker(secid):
     url_ticker = 'https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities/{}.json'
     response = requests.request('GET', url_ticker.format(secid))
     data = response.json()['securities']['data']
     return data[0][4]
 
-def futures_long(secid):
-    initialmargin, stepprice, minstep = get_future_ticker(secid)
-    sizeposition = round((initialmargin * VALUE_COUNT_CONTRACTS), 2)
-    stoplosspoint = round((VALUE_PRICE_OPEN_LONG - VALUE_PRICE_STOP_LOSS_LONG), 2)
-    potentialloss = round((stoplosspoint / minstep * stepprice * VALUE_COUNT_CONTRACTS), 2)
-    potentiallosspercent = round((potentialloss / VALUE_DEPOSIT * 100), 2)
+def return_stock_info(secid, _deposit, _count_contracts, _price_open, _price_stop):
+    lot = get_stock_ticker(secid)
+    sizeposition = round((lot * _count_contracts * _price_open), 2)
+    if _price_open > _price_stop:
+        stoplosspoint = round((_price_open - _price_stop), 2)
+    else:
+        stoplosspoint = round((_price_stop - _price_open), 2)
+    potentialloss = round((stoplosspoint * _count_contracts * _price_open), 2)
+    potentiallosspercent = round((potentialloss / _deposit * 100), 2)
     return {
-        'go': initialmargin,
+        'lot_size': lot,
         'count_positions': sizeposition,
         'stop_loss_p': stoplosspoint,
         'potential_loss': potentialloss,
         'percent_deposit': potentiallosspercent
     }
+
+def futures_long(secid):
+    return return_future_info(secid, VALUE_DEPOSIT, VALUE_COUNT_CONTRACTS, VALUE_PRICE_OPEN_LONG, VALUE_PRICE_STOP_LOSS_LONG)
 
 def futures_short(secid):
-    initialmargin, stepprice, minstep = get_future_ticker(secid)
-    sizeposition = round((initialmargin * VALUE_COUNT_CONTRACTS), 2)
-    stoplosspoint = round((VALUE_PRICE_STOP_LOSS_SHORT - VALUE_PRICE_OPEN_SHORT), 2)
-    potentialloss = round((stoplosspoint / minstep * stepprice * VALUE_COUNT_CONTRACTS), 2)
-    potentiallosspercent = round((potentialloss / VALUE_DEPOSIT * 100), 2)
-    return {
-        'go': initialmargin,
-        'count_positions': sizeposition,
-        'stop_loss_p': stoplosspoint,
-        'potential_loss': potentialloss,
-        'percent_deposit': potentiallosspercent
-    }
+    return return_future_info(secid, VALUE_DEPOSIT, VALUE_COUNT_CONTRACTS, VALUE_PRICE_OPEN_SHORT, VALUE_PRICE_STOP_LOSS_SHORT)
 
 def stocks_long(secid):
-    lot = get_stock_ticker(secid)
-    sizeposition = round((lot * VALUE_COUNT_CONTRACTS * VALUE_PRICE_OPEN_LONG), 2)
-    stoplosspoint = round((VALUE_PRICE_OPEN_LONG - VALUE_PRICE_STOP_LOSS_LONG), 2)
-    potentialloss = round((stoplosspoint * VALUE_COUNT_CONTRACTS * VALUE_PRICE_OPEN_LONG), 2)
-    potentiallosspercent = round((potentialloss / VALUE_DEPOSIT * 100), 2)
-    return {
-        'lot_size': lot,
-        'count_positions': sizeposition,
-        'stop_loss_p': stoplosspoint,
-        'potential_loss': potentialloss,
-        'percent_deposit': potentiallosspercent
-    }
+    return return_stock_info(secid, VALUE_DEPOSIT, VALUE_COUNT_CONTRACTS, VALUE_PRICE_OPEN_LONG, VALUE_PRICE_STOP_LOSS_LONG)
 
 def stocks_short(secid):
-    lot = get_stock_ticker(secid)
-    sizeposition = round((lot * VALUE_COUNT_CONTRACTS * VALUE_PRICE_OPEN_SHORT), 2)
-    stoplosspoint = round((VALUE_PRICE_STOP_LOSS_SHORT - VALUE_PRICE_OPEN_SHORT), 2)
-    potentialloss = round((stoplosspoint * VALUE_COUNT_CONTRACTS * VALUE_PRICE_OPEN_SHORT), 2)
-    potentiallosspercent = round((potentialloss / VALUE_DEPOSIT * 100), 2)
-    return {
-        'lot_size': lot,
-        'count_positions': sizeposition,
-        'stop_loss_p': stoplosspoint,
-        'potential_loss': potentialloss,
-        'percent_deposit': potentiallosspercent
-    }
+    return return_stock_info(secid, VALUE_DEPOSIT, VALUE_COUNT_CONTRACTS, VALUE_PRICE_OPEN_SHORT, VALUE_PRICE_STOP_LOSS_SHORT)
--- tests/run.bat
+++ tests/run.bat
@@ -1,3 +1,4 @@
 @echo off
 call ..\.venv\Scripts\Activate
 call pabot --processes 4 --outputdir Results TestCases\*.robot
+call pause
