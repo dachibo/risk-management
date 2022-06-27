aed6c64e1cb1cb962a4d9990e28865da8b58b50b
Danil <qoop.00@mail.ru>
2022-06-27 10:49:06 +0500

    fix input

--- src/components/field-input.js
+++ src/components/field-input.js
@@ -4,13 +4,26 @@ import React from "react";
 const FieldInput = ({id, name, value, editable = false, editValueField}) => {
     const dispatch = useDispatch();
     const mt = id === "go" || id === "lot_size" ? null : "mt-6"
+
+    const ItemHandlerChange = (e) => {
+        dispatch(editValueField(e.target.value * 1))
+    };
+
+    const KeyPressHandler = (e) => {
+        if (!/[0-9]|[,|.]/.test(e.key)) {
+            e.preventDefault();
+          }
+    };
+
     return (
         <div className={`${mt} relative`}>
             <input id={id} key={id} type="number"
                    className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-600"
-                   placeholder=" " value={value} disabled={editable} onChange={(e) => {
-                dispatch(editValueField(e.target.value * 1))
-            }}/>
+                   placeholder=" " 
+                   value={value} 
+                   disabled={editable}
+                   onKeyPress={KeyPressHandler} 
+                   onChange={ItemHandlerChange}/>
             <label htmlFor={id}
                    className="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">
                 {name}
--- src/components/field-search.js
+++ src/components/field-search.js
@@ -1,6 +1,5 @@
 import {useDispatch} from "react-redux";
 import {setAllNull} from "../store/dataReducer";
-import {DebounceInput} from "react-debounce-input";
 import React, {useState} from "react";
 import {Autocomplete} from "./autocomplete";
 
@@ -31,18 +30,10 @@ const FieldSearch = () => {
             <input 
             id="search_ticker" 
             type="search" 
-            class="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-600" 
+            className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-600" 
             placeholder=" " 
             value={shortName}
             onChange={searchInputHandler}/>
-            {/* <DebounceInput
-                id="search_ticker" type="search"
-                className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-600"
-                placeholder=" "
-                value={shortName}
-                minLength={2}
-                debounceTimeout={300}
-                onChange={searchInputHandler}/> */}
             <label htmlFor="search_ticker"
                    className="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">
                 Поиск тикера
